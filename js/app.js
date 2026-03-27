96203e2ec7cceecdbb0dbbacabe6b6e3d92aac89
mrsmlisa <lisa.q.scott@gmail.com>
2026-03-27 12:17:54 +0100

    small changes in html and css. add code for caroulse in js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 
 
+let imageSlide = document.getElementsByClassName("carousel-image")
+
+for (i=0; i < imageSlide.length; i++) {
+    imageSlide[i].style.display = "none"
+}
+imageSlide[0].style.display= "block"
+
+let currentImage = 0
+
+function next() {
+    imageSlide[currentImage].style.display = "none"
+    imageSlide[currentImage++].style.display = "block"
+}
